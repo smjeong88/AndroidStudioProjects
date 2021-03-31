16d36b8f755fd92c7fe88d256413178d93f9915c
smjeong88 <smvtr123@naver.com>
2021-04-01 01:31:35 +0900

    first commit

---
+++ ListExample01/app/src/main/java/com/example/listexample01/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.listexample01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        list = (ListView)findViewById(R.id.list);
+
+        List<String> data = new ArrayList<>();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,data);
+        list.setAdapter(adapter);
+
+        data.add("정세민");
+        data.add("홍드로이드");
+        data.add("안드로이드");
+        data.add("스타벅스");
+        adapter.notifyDataSetChanged();
+    }
+}
